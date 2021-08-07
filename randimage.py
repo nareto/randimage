c86a11c51357c2bc6a12c99fbcaabcf273a421bc
bnh <bnh@mars>
2021-08-07 13:14:23 +0200

    idea

---
+++ randimage.py
@@ -0,0 +1,4 @@
+#1 generate random grayvalue n x n mask
+#2 pick random starting point
+#3 create region-filling path by using cellular-automata-like rule based on mask 
+#4 pick random matplotlib colormap and color path from first to last point using full range of colormap
