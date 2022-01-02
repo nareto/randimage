9059b585192da56f596d3352e0d50461361c4f8b
bnh <bnh@mars>
2022-01-02 17:48:33 +0100

    bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="randimage",
-    version="1.1",
+    version="1.2",
     description="Creates random images",
     long_description=README,
     long_description_content_type="text/markdown",
